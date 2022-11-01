3a6eb43ef4fe74be8d653ec3e1887df42fba3698
Sally Young <git@justa.fish>
2022-11-01 14:33:14 +0000

    Revert old versions

--- tests/fixtures/phpunit/testmodule/tests/src/ExistingSite/ExampleTest.php
+++ tests/fixtures/phpunit/testmodule/tests/src/ExistingSite/ExampleTest.php
@@ -12,7 +12,7 @@ use weitzman\DrupalTestTraits\ExistingSiteBase;
 class ExampleTest extends ExistingSiteBase
 {
 
-    protected function setUp(): void
+    protected function setUp()
     {
         parent::setUp();
 
@@ -29,31 +29,17 @@ class ExampleTest extends ExistingSiteBase
      */
     public function testLlama()
     {
-        // Creates a user. Will be automatically cleaned up at the end of the test.
-        $author = $this->createUser([], null, true);
-
-        // Create a taxonomy term. Will be automatically cleaned up at the end of the test.
-        $vocab = Vocabulary::load('tags');
-        $term = $this->createTerm($vocab);
-
         // Create a "Llama" article. Will be automatically cleaned up at end of test.
         $node = $this->createNode([
             'title' => 'Llama',
-            'type' => 'article',
-            'field_tags' => [
-                'target_id' => $term->id(),
-            ],
-            'uid' => $author->id(),
+            'type' => 'page',
+            'uid' => 1,
         ]);
         $node->setPublished()->save();
-        $this->assertEquals($author->id(), $node->getOwnerId());
+        $this->assertEquals(1, $node->getOwnerId());
 
         // We can browse pages.
         $this->drupalGet($node->toUrl());
         $this->assertSession()->statusCodeEquals(200);
-
-        // We can login and browse admin pages.
-        $this->drupalLogin($author);
-        $this->drupalGet($node->toUrl('edit-form'));
     }
 }
